fd40be1b1bac834a4073e906bda6b69f96aa62c5
Tim Busbee <tim@dotedison.com>
2019-11-05 19:28:00 -0600

    Testing

--- src/ios/PushbotsPlugin.m
+++ src/ios/PushbotsPlugin.m
@@ -24,9 +24,8 @@ static char launchNotificationKey;
 
         dispatch_async(dispatch_get_main_queue(), ^{
             //Ask for Push permission && create Pushbots sharedInstance
-            [Pushbots initWithAppId:appId withLaunchOptions:nil prompt:true receivedNotification:^(NSDictionary *result) {
-                NSLog(@"receivedNotification: ", result);
-            } openedNotification:^(NSDictionary *result) {
+            NSLog(@"PBTEST Just Testing");
+            [Pushbots initWithAppId:appId withLaunchOptions:nil prompt:true receivedNotification:nil openedNotification:^(NSDictionary *result) {
                 [self notificationOpened:result];
             }];
         });
