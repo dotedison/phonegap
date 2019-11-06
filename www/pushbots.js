ee6fa600caf00a1f29bbfe1c52e07e24f9f65492
Tim Busbee <tim@dotedison.com>
2019-11-05 19:01:30 -0600

    Testing

--- www/pushbots.js
+++ www/pushbots.js
@@ -35,12 +35,12 @@ PushbotsPlugin.prototype.initialize = function(app_id, options) {
 	/* VALIDATE APP_ID */
 	// Pushbots Application ID is required
 	if (typeof app_id === 'undefined') {
-		console.error('app_id argument is required.');
+		console.log('app_id argument is required.');
 	}
 
 	var checkForAppId = new RegExp("^[0-9a-fA-F]{24}$");
 	if (!checkForAppId.test(app_id)) {
-		console.error('app_id argument is not valid.');
+		console.log('app_id argument is not valid.');
 	}
 
 	this.app_id = app_id;
@@ -52,6 +52,7 @@ PushbotsPlugin.prototype.initialize = function(app_id, options) {
 	
 	var that = this;
 	var success = function(data){
+		console.log('PBTEST success:', JSON.stringify(data));
 		if (data && typeof data.type !== 'undefined') {
 			//Registration event
 			if(data.type === "registered"){
@@ -75,7 +76,7 @@ PushbotsPlugin.prototype.initialize = function(app_id, options) {
 	};
 	
 	var fail = function(error){
-		console.error(error);
+		console.log('PBTEST error: ', JSON.stringify(error));
 	};
     //Intialize Pushbots
 	exec(success, fail, SERVICE_TITLE, 'initialize', [this.app_id, this.options]);
